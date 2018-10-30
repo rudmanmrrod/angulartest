8bf8fe13ff1532e82546a49de9991d9c3ac0a8fe
Rudmanmrrod <rudmanmrrod@gmail.com>
2018-10-30 11:56:35 -0400

    Añadido consumo de api de github

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-table',
@@ -6,27 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-	content_table: any = {
-  		codigo: '0001',
-	    name: 'producto1',
-	    precio_minimo: 500,
-	    precio_cliente: 600
-  };
-	lista: any = [];
+	repos: any = [];
 
-  constructor() {
-  	this.lista.push(this.content_table)
-  	this.content_table = {
-  		codigo: '0102',
-	    name: 'producto2',
-	    precio_minimo: 500,
-	    precio_cliente: 5500
-  	}
-  	this.lista.push(this.content_table)
-  	console.log(this.lista)
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
+    this.http.get('https://api.github.com/users/rudmanmrrod/repos').subscribe(response => {
+      this.repos = response
+    })
   }
 
 }
